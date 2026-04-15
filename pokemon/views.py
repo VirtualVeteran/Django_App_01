205bb30050f8a98852348bc356fd83c706e661bb
virtualveteran <estheroswald001@yahoo.com>
2026-04-15 15:54:27 -0600

    adds functions into views.py

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,9 +1,14 @@
-
-
-# Create your views here.
 # pokemon/views.py
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 
-def hello(request):
-    return HttpResponse("Hello from Django!")
+POKEMON_DATA = {
+    "pikachu": {"name": "pikachu", "type": "electric", "base_experience": 112},
+    "bulbasaur": {"name": "bulbasaur", "type": "grass", "base_experience": 64},
+    "charmander": {"name": "charmander", "type": "fire", "base_experience": 62},
+}
+
+
+def pokemon_list(request):
+    data = list(POKEMON_DATA.values())
+    return JsonResponse(data, safe=False)
